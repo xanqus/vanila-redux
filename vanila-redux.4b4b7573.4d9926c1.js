3130048bd0137e7ac9f66e470f7b62d5b9525c21
xanqus <sbk98031816@gmail.com>
2021-03-04 18:41:30 +0900

    v 1

---
+++ vanila-redux.4b4b7573.4d9926c1.js
@@ -0,0 +1,2 @@
+console.log("hello parcel");
+//# sourceMappingURL=vanila-redux.4b4b7573.4d9926c1.js.map
---
+++ vanila-redux.4b4b7573.js
@@ -0,0 +1,2 @@
+console.log("hello parcel");
+//# sourceMappingURL=vanila-redux.4b4b7573.js.map
